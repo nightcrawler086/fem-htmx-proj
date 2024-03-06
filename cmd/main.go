405d655777635fa6aef9f5646f0a14843fd78c6d
nightcrawler086 <brian.hall4@gmail.com>
2024-03-05 23:08:50 -0500

    deletes

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
-package main 
+package main
 
-import ( 
-  "text/template" 
-  "io"
-  "fmt"
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
+import (
+	"io"
+	"strconv"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -22,15 +23,19 @@ func newTemplate() *Templates {
   }
 }
 
+var id = 0
 type Contact struct {
   Name string
   Email string
+  Id int
 }
 
 func newContact(name string, email string) Contact {
+  id++
   return Contact{
     Name: name,
     Email: email,
+    Id: id,
   }
 }
 
@@ -38,6 +43,15 @@ type Contacts = []Contact
 
 type Data struct {
   Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+  for i, contact := range d.Contacts {
+    if contact.Id == id {
+      return i
+    }
+  }
+  return -1
 }
 
 func (d *Data) hasEmail(email string) bool {
@@ -107,7 +121,6 @@ func main() {
       formData.Values["email"] = email
       formData.Errors["email"] = "Email already exists"
 
-      fmt.Println("found duplicate contact")
       return c.Render(422, "form", formData)
     }
 
@@ -116,6 +129,22 @@ func main() {
 
     c.Render(200, "form", newFormData())
     return c.Render(200, "oob-contact", contact)
+  })
+
+  e.DELETE("/contacts/:id", func(c echo.Context) error {
+    idStr := c.Param("id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+      return c.String(400, "Invalid ID")
+    }
+
+    index := page.Data.indexOf(id)
+    if index == -1 {
+      return c.String(404, "Contact not found")
+    }
+    page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+    return c.NoContent(200)
   })
   
   e.Logger.Fatal(e.Start(":42069"))
